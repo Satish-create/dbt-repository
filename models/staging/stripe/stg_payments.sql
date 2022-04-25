a3b853f32a22bc3b691f0682a748ea14770ea341
satish.adda@criticalriver.com <satish.adda@criticalriver.com>
2022-04-25 11:32:27 +0000

    created sources and tests

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,13 @@
+with payments as (
+    select
+         id as payment_id,
+         orderid as order_id,
+         paymentmethod as payment_method,
+         status,
+         amount,
+         created as created_date
+    /*from raw.stripe.payment*/
+    from {{ source('stripe','payment')}}
+)
+
+select * from payments
